aa8898246753dcdc7265572707b11c0fee5fab7a
Tony Bowden <tony@tmtm.com>
2019-01-25 11:11:08 +0000

    Adjust for changed layout

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,7 @@ names_6 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/6th_Legislative_Council_of_Hong_Kong',
   after: '//span[@id="List_of_members"]',
   before: '//span[@id="Committees"]',
-  xpath: '//table//tr[td]/td[4]//a[not(@class="new")]/@title',
+  xpath: '//table//tr[td]/td[5]//a[not(@class="new")]/@title',
 )
 
 sparq = <<~SPARQL
